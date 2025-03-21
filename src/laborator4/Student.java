5e9b078621119c4e01ac002d0336c3cd6c6d6978
Cristina <mariacristina.hategan@ulbsibiu.ro>
2025-03-21 09:34:39 +0200

    Laborator 4

---
+++ src/laborator4/Student.java
@@ -0,0 +1,36 @@
+package laborator4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String nume;
+    private String grupa;
+    private List<Integer> note;
+
+    public Student(String nume, String grupa){
+        this.nume = nume;
+        this.grupa = grupa;
+        note = new ArrayList();
+    }
+    public String getNume() {
+        return nume;
+    }
+
+    public String getGrupa() {
+        return grupa;
+    }
+
+    public void adaugaNota(int nota) {
+        note.add(nota);
+    }
+
+    public boolean esteIntegralist() {
+        return note.stream().allMatch(n -> n >= 5);
+    }
+
+    public float getMedie() {
+        return (float) note.stream().mapToInt(Integer::intValue).sum() / note.size();
+    }
+
+}
